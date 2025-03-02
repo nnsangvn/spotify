0ff2b047c1ca13d8786dab38b4dc4acc07c3de8d
Nguyễn Nhất Sang <nnsang@192.168.1.149.non-exists.ptr.local>
2025-03-02 23:49:32 +0700

    get started page

---
+++ lib/common/widgets/button/basic_app_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class BasicAppButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String title;
+  final double? height;
+
+  const BasicAppButton({
+    required this.onPressed,
+    required this.height,
+    required this.title,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size.fromHeight(height ?? 92),
+      ),
+      child: Text(title),
+    );
+  }
+}
---
+++ lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  AppImages.introBG,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black.withValues(alpha: 0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                Spacer(),
+                Text(
+                  'Enjoy Listening To Music',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 21),
+                Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.',
+                  style: TextStyle(
+                    color: AppColors.grey,
+                    fontSize: 13,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                BasicAppButton(
+                  onPressed: () {},
+                  height: 90,
+                  title: "Get Started",
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
